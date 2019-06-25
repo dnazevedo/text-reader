3ed05abf369deaae774a52202f33bf6b2bd5fe6b
Daiane <daianeneves_@hotmail.com>
2019-06-24 23:26:02 -0300

    WIP

---
+++ TextReader/LeroLeroService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+
+namespace TextReader
+{
+    public class LeroLeroService
+    {
+        public HttpClient Client { get; }
+
+        public LeroLeroService(HttpClient client)
+        {
+            client.BaseAddress = new System.Uri("https://www.lerolero.com/");
+        }
+    }
+}
---
+++ TextReader/Program.cs
@@ -0,0 +1,50 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Polly;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TextReader
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var client = new HttpClient();
+
+            var siteText = "";
+
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                var policy = Policy<string>
+                   .Handle<Exception>()
+                   .Fallback(() => blabla());
+
+                driver.Navigate().GoToUrl("https://www.lipsum.com//");
+                var element = driver.FindElement(By.XPath("//div[@id='Panes']/div[1]/p"));
+                Console.WriteLine(element.Text);
+                siteText = element.Text;
+
+                driver.Navigate().GoToUrl("https://mothereff.in/byte-counter");
+
+                var inputText = driver.FindElement(By.TagName("textarea"));
+                inputText.Clear();
+                inputText.SendKeys(siteText);
+
+                var numberOfBytes = driver.FindElement(By.Id("bytes"));
+
+                Console.WriteLine(numberOfBytes.Text);
+            }
+
+            Console.ReadKey();
+        }
+
+        public static string blabla()
+        {
+            return "";
+        }
+    }
+}
